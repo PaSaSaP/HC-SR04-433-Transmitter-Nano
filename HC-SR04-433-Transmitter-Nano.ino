b0500cd03d12fc484d95077ac36201f9f00694d5
pasasap <pasasap.gh@outlook.com>
2022-09-12 11:54:54 +0200

    Change msg structure. Fix CRC setting.

--- HC-SR04-433-Transmitter-Nano.ino
+++ HC-SR04-433-Transmitter-Nano.ino
@@ -1,14 +1,10 @@
-//#include <kd_HysteresisValue.h>
-#include <kd_MeanValue.h>
 #include <HCSR04.h>
 #include <RH_ASK.h>
 #include <CRC16_c.h>
+#include "RfData.h"
 
 bool constexpr SerialEnabled = true;
 
-#define TriggerPin 4
-// uint8_t const EchoPins[] {5, 6, 7, 8};
-uint8_t const EchoPins[] {5, 6};
 uint32_t currentTime;
 uint32_t triggerTime;
 RH_ASK driver(2000, 11, 9);
@@ -23,19 +19,13 @@ void setup() {
   if (!driver.init()) {
     if (SerialEnabled) Serial.println("driver failed");
   }
+  CRC16_reset();
 }
-
-struct RfData {
-  uint16_t begin = 0x1212;
-  double results[sizeof(EchoPins) / sizeof(*EchoPins)];
-  uint16_t crc;
-  uint16_t end = 0xEFEF;
-} rfData;
 
 void updateCrc() {
   CRC16_restart();
-  auto data = reinterpret_cast<uint8_t const*>(rfData.results);
-  auto length = sizeof(rfData.results);
+  auto data = reinterpret_cast<uint8_t const*>(&rfData.msgId);
+  auto length = sizeof(rfData) - sizeof(rfData.crc);
   while (length--) {
     CRC16_add(*data++);
   }
@@ -43,27 +33,29 @@ void updateCrc() {
 }
 
 void sendRfData() {
-  // driver.send("abcd\n", 6);
   driver.send((uint8_t *)&rfData, sizeof(rfData));
   driver.waitPacketSent();
 }
 
-void measureDistances() {
-  if (SerialEnabled) Serial.println("start measurement");
-  auto t1 = millis();
-  HCSR04.measureDistanceCm(rfData.results);
-  t1 = millis() - t1;
+double results[EchoPinCount];
 
-  if (SerialEnabled) Serial.print("pomiar zajal: "); 
+void measureDistances() {
+  if (SerialEnabled) Serial.println("start meas");
+  auto t1 = millis();
+  HCSR04.measureDistanceMm(results);
+  t1 = millis() - t1;
+  rfData.msgId = 0x10 + EchoPinCount;
+  for (size_t i = 0; i < EchoPinCount; ++i) {
+    rfData.results[i] = results[i];
+  }
+
+  if (SerialEnabled) Serial.print("meas took: "); 
   if (SerialEnabled) Serial.print(t1);
   if (SerialEnabled) Serial.print("ms, D: [");
-  if (SerialEnabled) Serial.print(rfData.results[0]);
-  if (SerialEnabled) Serial.print(", ");
-  if (SerialEnabled) Serial.print(rfData.results[1]);
-  // if (SerialEnabled) Serial.print(", ");
-  // if (SerialEnabled) Serial.print(rfData.results[2]);
-  // if (SerialEnabled) Serial.print(", ");
-  // if (SerialEnabled) Serial.print(rfData.results[3]);
+  for (size_t i = 0; i < EchoPinCount; ++i) {
+    if (SerialEnabled) Serial.print(rfData.results[i]);
+    if (SerialEnabled) Serial.print(", ");
+  }
   if (SerialEnabled) Serial.println("]");
   updateCrc();
   sendRfData();
---
+++ RfData.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_RFDATA_H_
+#define HEADER_RFDATA_H_
+
+#define TriggerPin 4
+// uint8_t const EchoPins[] {5, 6, 7, 8};
+uint8_t const EchoPins[] {5, 6};
+size_t constexpr EchoPinCount = sizeof(EchoPins) / sizeof(*EchoPins);
+
+struct RfData {
+  uint16_t crc;
+  uint16_t msgId;
+  uint16_t results[EchoPinCount];
+  uint16_t end = 0xEFEF;
+} rfData;
+
+#endif /* HEADER_RFDATA_H_ */
+
